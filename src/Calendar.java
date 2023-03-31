8be421968596b0f4d3b7f14900c1d4bb5d3b3ad9
Raouf Medjadj <lm_medjadj@esi.dz>
2023-03-31 19:13:45 +0100

    added calendar, day and planning general logic

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,21 +1,48 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Calendar {
     // each user is bonded with one calendar and vice-versa
     private User user;
     // a calendar should contains all plannings
-    private ArrayList<Planning> plannings;
-    // all days contained in all plannings should be grouped in one collection (did not decide the collection specefic child class yet)
-    private Collection<Day> days;
+    private ArrayList<Planning> plannings = new ArrayList<>();
+    private TreeMap<LocalDate, Day> calendar = new TreeMap<>();
 
-    public void addPlanning(Planning planning){
-        // make all the necessary checks before adding the planning using the ArrayList adding method (use the predefined "add" method)
+
+    public void addDay(Day day) {
+        calendar.putIfAbsent(day.getDate(), day);
     }
-    public void removePlanning(Planning planning){
+
+    public void addPlanning(Planning planning) {
+        Day day;
+        // plannings can override, so there are no restriction to how plannings are added
+        if (!planning.getStartDay().isBefore(LocalDate.now()) && !planning.getEndDay().isBefore(planning.getStartDay())) {
+            for (LocalDate date = planning.getStartDay(); !date.isAfter(planning.getEndDay()); date = date.plusDays(1)) {
+                if (!calendar.containsKey(date)) {
+                    day = new Day(date);
+                    planning.addDay(day);
+                    addDay(day); //  add to the calendar
+                }
+                else planning.addDay(calendar.get(date));
+            }
+            plannings.add(planning);
+
+        }
+        else System.out.println("ERREUR: Période de planning invalide!");
+    }
+    public void removePlanning(Planning planning) {
         // check whether the planning exists (using it's ID, and then removing it using the predefined method, must have a user validation before ...)
     }
-    public void extendPlanning(Planning planning){
+    public void extendPlanning(Planning planning) {
         //check first that the given planning is extendable, if so then call : planning.extendPlanning
+    }
+
+    //temp
+    public void showCalendar() {
+        for (Map.Entry<LocalDate, Day> entry : calendar.entrySet()) {
+            System.out.println(entry.getKey() + " ->  " + entry.getValue());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
+        // We need a way to store user info on the machine, eg. a file.
+//        app.signup("ok", "123");
+//        app.login("ok", "123");
 
-        app.signup("ok", "123");
-        app.signup("okkk", "12");
-        app.signup("ok", "123");
-        app.login("okkkk", "123");
-        app.login("ok", "123");
-        app.login("okkk", "123");
-        app.login("okkk", "12");
+        Calendar calendar = new Calendar();
+        calendar.addDay(new Day(LocalDate.now()));
+        calendar.addDay(new Day(LocalDate.of(1990, 12, 12)));
+        calendar.addDay(new Day(LocalDate.of(1995, 12, 12)));
+        calendar.addDay(new Day(LocalDate.of(2023, 4, 12)));
+        calendar.addDay(new Day(LocalDate.of(2023, 4, 13)));
+        calendar.addDay(new Day(LocalDate.of(2023, 4, 14)));
+        Planning planning1 = new Planning(LocalDate.now(), LocalDate.of(2023, 4, 23), calendar);
+
+        calendar.addPlanning(planning1);
+        calendar.showCalendar();
+        System.out.println(" ----------------");
+        planning1.showPlanning();
     }
 }
